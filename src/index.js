dc12d39c6f530087a1929e836c1738ec420b9971
Yury <42602312+diesel167@users.noreply.github.com>
2020-02-17 21:40:44 +0200

    fix: fixed bug 'abba'

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 function decrypt(){
-  let textarea = document.getElementById('encrypted').value.split('');  
-  for(let i = 0; i < textarea.length-1; i++){
-    if(textarea[i] === textarea[i+1]){
-      textarea.splice(i, 2);
-      i--;
-    }
+  let textarea = document.getElementById('encrypted').value.split('');
+  let isFinished = false;
+  let anyChanges = false;
+  while(!isFinished){
+     textarea.map((symbol,i) => {
+       if(i < textarea.length-1 && symbol === textarea[i+1]){
+         textarea.splice(i,1);
+         textarea.splice(i,1);
+         anyChanges = true;
+       }
+     });
+     if(!anyChanges){
+       isFinished = true;
+     } 
+     anyChanges = false;
   }
   document.getElementById('answer').innerHTML = textarea.join('');
 }
